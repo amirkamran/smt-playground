53d7ed18895484628eeaf2c169f3bcbbb2285d0a
bojar <bojar@85e76b90-d0e1-47da-8fde-e7818187306d>
2009-07-21 07:08:48 +0000

    moved specific rules of summarizer to a separate file
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/statmt/trunk@791 85e76b90-d0e1-47da-8fde-e7818187306d

---
+++ playground/summarizer.enhi.config.pl
@@ -0,0 +1,433 @@
+# This is loaded by summarizer.pl to create beautiful tables of en->hi results
+
+my $beautify = {
+      "2051" => "out-of-domain-D",
+      "964" => "domain-D",
+      "LM.*LM" => "two-LMs",
+      "lc\\+2" => "tag",
+      "TGTlc[^.]*\\+2" => "tag",
+    };
+
+my $valembeautify = {
+      "t0-0DEV" => "no-reord",
+      "di.r0-0DEV" => "no-reord",
+      "1130" => "FIXED",
+      "or-bi-fe.r0-0" => "reord-using-hi-forms",
+      "or-bi-fe.r0-1" => "reord-using-wcX/dzsuf",
+      "or-bi-fe.r1-1" => "reord-using-entags-hiwcX/dzsuf",
+      "hi\\." => "baseline",
+      "\\+en\\+" => "baseline",
+      "\\+enP\\+" => "penntok",
+      "\\+enR\\+" => "penntok+dzReord",
+    };
+
+my @scans = (
+  [ "---------------------------------------",
+    "", "Just a delimiter line, below are tables for experimenting with valem",
+    "", 4, " ", " nic ", "nic", 0, $valembeautify, ],
+  [
+    "Alignment",
+    "",
+    # required
+    "
+    hi
+    ALI
+    ",
+    # forbidden
+    "
+    enR
+    tides web eiti allFIX
+    wc dzsuf
+    devnormal
+    DEVicon-tides
+    ALIicon-all
+    devnorm
+    ",
+    1,
+    # rows
+    "
+    (ALI[^.]+)
+     ",
+    # cols
+    "
+    web
+    tides
+    SRCicon-([a-zA-Z]+)
+    or-bi-fe.r([01]-[01])
+    ",
+    # sort
+    "SRCicon-eilmt",
+    0, # verbose
+    $valembeautify,
+  ],
+  [
+    "Parallel corpus size-simple-mtevalBLEU",
+    "",
+    # required
+    "
+    hi
+    ALIlcstem4-lcstem4
+    ",
+    # forbidden
+    "
+    enR
+    tides web
+    wc dzsuf
+    devnormal
+    DEVicon-tides
+    ALIicon-all
+    ",
+    3,
+    # rows
+    "
+    web
+    tides
+    SRCicon-([a-zA-Z]+)
+     ",
+    # cols
+    "
+    or-bi-fe.r([01]-[01])
+    t0-0DEV
+    ",
+    # sort
+    "no-reord",
+    0, # verbose
+    $valembeautify,
+  ],
+  [
+    "Parallel corpus size-simple",
+    "",
+    # required
+    "
+    hi
+    ALIlcstem4-lcstem4
+    ",
+    # forbidden
+    "
+    enR
+    tides web
+    wc dzsuf
+    devnormal
+    DEVicon-tides
+    ALIicon-all
+    ",
+    1,
+    # rows
+    "
+    web
+    tides
+    SRCicon-([a-zA-Z]+)
+     ",
+    # cols
+    "
+    or-bi-fe.r([01]-[01])
+    t0-0DEV
+    ",
+    # sort
+    "no-reord",
+    0, # verbose
+    $valembeautify,
+  ],
+  [
+    "LM corpus size-simple-mteval",
+    "",
+    # required
+    "
+    ALIlcstem4-lcstem4
+    DEVicon-eilmt
+    ",
+    # forbidden
+    "
+    enR
+    tides
+    eiti
+    wc dzsuf
+    devnormal
+    DEVicon-tides
+    SRCicon-all\\+
+    ",
+    3,
+    # rows
+    "
+    SRCicon-([a-zA-Z0-9]+)
+    web
+     ",
+    # cols
+    "
+    or-bi-fe.r([01]-[01])
+    t0-0DEV di.r0-0DEV
+    ",
+    # sort
+    "reord-using-hi-forms",
+    0, # verbose
+    $valembeautify,
+  ],
+  [
+    "LM corpus size-simple",
+    "",
+    # required
+    "
+    ALIlcstem4-lcstem4
+    DEVicon-eilmt
+    ",
+    # forbidden
+    "
+    enR
+    tides
+    eiti
+    wc dzsuf
+    devnormal
+    DEVicon-tides
+    SRCicon-all\\+
+    ",
+    1,
+    # rows
+    "
+    SRCicon-([a-zA-Z0-9]+)
+    web
+     ",
+    # cols
+    "
+    or-bi-fe.r([01]-[01])
+    t0-0DEV di.r0-0DEV
+    ",
+    # sort
+    "reord-using-hi-forms",
+    0, # verbose
+    $valembeautify,
+  ],
+  [
+    "Parallel corpus size",
+    "",
+    # required
+    "
+    hi
+    ",
+    # forbidden
+    "
+    wc dzsuf
+    devnormal
+    DEVicon-tides
+    ",
+    1,
+    # rows
+    "
+    (ALI[^.]+)
+    LM([0-9]+-[0-9]+)
+    t0-0\\+t1-0
+    t0-0\\.
+    web
+    tides
+    or-bi-fe.r([01]-[01])
+     ",
+    # cols
+    "
+    SRCicon-([a-zA-Z]+)
+    ",
+    # sort
+    "SRCicon-eilmt",
+    0, # verbose
+    $valembeautify,
+  ],
+  [
+    "Normalization",
+    "",
+    # required
+    "
+    hi
+    ",
+    # forbidden
+    "
+    wc dzsuf
+    DEVicon-tides
+    ",
+    1,
+    # rows
+    "
+    SRCicon-([a-zA-Z]+)
+    (ALI[^.]+)
+    LM([0-9]+-[0-9]+)
+    web
+    tides
+    t0-0\\+t1-0
+    t0-0\\.
+    or-bi-fe.r([01]-[01])
+     ",
+    # cols
+    "
+    (hi\\.|devnormal2?)
+    ",
+    # sort
+    "hi.",
+    0, # verbose
+    $valembeautify,
+  ],
+  [
+    "Reorderings",
+    "",
+    # required
+    "
+    SRCicon-eilmt
+    lcstem4-lcstem4
+    ",
+    # forbidden
+    "
+    devnormal
+    web tides
+    Exiting
+    ",
+    1,
+    # rows
+    "
+    (ALI[^.]+)
+    LM([0-9]+-[0-9]+)
+    or-bi-fe.r([01]-[01])
+     ",
+    # cols
+    "
+    hi\\.
+    dzsuf
+    wc10
+    wc50
+    1130
+    ",
+    # sort
+    "SRCicon-eilmt",
+    0, # verbose
+    $valembeautify,
+  ],
+  [
+    "Reorderings - for paper",
+    "",
+    # required
+    "
+    lcstem4-lcstem4
+    ",
+    # forbidden
+    "
+    wc[15]0.*112[89]
+    LM1-[357]
+    devnormal
+    web 
+    Exiting
+    wc10 wc50
+    all eiti
+    LM0-3-icon-eilmtLM0-3-icon-tides
+    ",
+    1,
+    # rows
+    "
+    \\+en([RP]?)\\+
+    or-bi-fe.r([01]-[01])
+    dzsuf
+     ",
+    # cols
+    "
+    DEVicon-tides
+    DEVicon-eilmt
+    ",
+    # sort
+    "DEVicon-eilmt",
+    0, # verbose
+    undef,
+  ],
+  [
+    "Reorderings - simplified view, eilmt",
+    "",
+    # required
+    "
+    SRCicon-eilmt
+    lcstem4-lcstem4
+    ",
+    # forbidden
+    "
+    wc[15]0.*112[89]
+    LM1-[357]
+    devnormal
+    web tides
+    Exiting
+    ",
+    1,
+    # rows
+    "
+    \\+en([RP]?)\\+
+    or-bi-fe.r([01]-[01])
+     ",
+    # cols
+    "
+    hi\\.
+    dzsuf
+    wc10
+    wc50
+    ",
+    # sort
+    "SRCicon-eilmt",
+    0, # verbose
+    $valembeautify,
+  ],
+  [
+    "Reorderings - tides",
+    "",
+    # required
+    "
+    SRCicon-tides
+    lcstem4-lcstem4
+    ",
+    # forbidden
+    "
+    web
+    eiti
+    ",
+    1,
+    # rows
+    "
+    \\+en([RP]?)\\+
+    or-bi-fe.r([01]-[01])
+     ",
+    # cols
+    "
+    hi\\.
+    dzsuf
+    wc10
+    wc50
+    ",
+    # sort
+    "baseline",
+    0, # verbose
+    $valembeautify,
+  ],
+  [
+    "Reorderings - tides, trained on eiti",
+    "",
+    # required
+    "
+    SRCicon-eiti
+    lcstem4-lcstem4
+    DEVicon-tides
+    ",
+    # forbidden
+    "
+    web
+    ",
+    1,
+    # rows
+    "
+    (ALI[^.]+)
+    or-bi-fe.r([01]-[01])
+     ",
+    # cols
+    "
+    hi\\.
+    dzsuf
+    wc10
+    wc50
+    ",
+    # sort
+    "baseline",
+    0, # verbose
+    $valembeautify,
+  ],
+);
+
+# This is the main output:
+\@scans;
--- playground/summarizer.pl
+++ playground/summarizer.pl
@@ -1,443 +1,50 @@
 #!/usr/bin/perl
+# Convert a huge listing of all results into several nice 2D tables.
+# The conversion rules are loaded from a given config file.
 
 use strict;
 use Summarizer;
+use File::Basename;
+use File::Path;
+use File::Spec;
+use Getopt::Long qw(GetOptionsFromString);
 
-my $data = Summarizer::load(*STDIN);
-    
-my $beautify = {
-      "2051" => "out-of-domain-D",
-      "964" => "domain-D",
-      "LM.*LM" => "two-LMs",
-      "lc\\+2" => "tag",
-      "TGTlc[^.]*\\+2" => "tag",
-    };
+binmode(STDOUT, ":utf8");
+binmode(STDERR, ":utf8");
+binmode(STDIN, ":utf8");
 
-my $valembeautify = {
-      "t0-0DEV" => "no-reord",
-      "di.r0-0DEV" => "no-reord",
-      "1130" => "FIXED",
-      "or-bi-fe.r0-0" => "reord-using-hi-forms",
-      "or-bi-fe.r0-1" => "reord-using-wcX/dzsuf",
-      "or-bi-fe.r1-1" => "reord-using-entags-hiwcX/dzsuf",
-      "hi\\." => "baseline",
-      "\\+en\\+" => "baseline",
-      "\\+enP\\+" => "penntok",
-      "\\+enR\\+" => "penntok+dzReord",
-    };
+my $mypath = File::Spec->rel2abs(__FILE__);
+my $basedir = dirname($mypath);
+my $scriptname = basename($mypath);
 
-my @scans = (
-  [ "---------------------------------------",
-    "", "Just a delimiter line, below are tables for experimenting with valem",
-    "", 4, " ", " nic ", "nic", 0, $valembeautify, ],
-  [
-    "Alignment",
-    "",
-    # required
-    "
-    hi
-    ALI
-    ",
-    # forbidden
-    "
-    enR
-    tides web eiti allFIX
-    wc dzsuf
-    devnormal
-    DEVicon-tides
-    ALIicon-all
-    devnorm
-    ",
-    1,
-    # rows
-    "
-    (ALI[^.]+)
-     ",
-    # cols
-    "
-    web
-    tides
-    SRCicon-([a-zA-Z]+)
-    or-bi-fe.r([01]-[01])
-    ",
-    # sort
-    "SRCicon-eilmt",
-    0, # verbose
-    $valembeautify,
-  ],
-  [
-    "Parallel corpus size-simple-mtevalBLEU",
-    "",
-    # required
-    "
-    hi
-    ALIlcstem4-lcstem4
-    ",
-    # forbidden
-    "
-    enR
-    tides web
-    wc dzsuf
-    devnormal
-    DEVicon-tides
-    ALIicon-all
-    ",
-    3,
-    # rows
-    "
-    web
-    tides
-    SRCicon-([a-zA-Z]+)
-     ",
-    # cols
-    "
-    or-bi-fe.r([01]-[01])
-    t0-0DEV
-    ",
-    # sort
-    "no-reord",
-    0, # verbose
-    $valembeautify,
-  ],
-  [
-    "Parallel corpus size-simple",
-    "",
-    # required
-    "
-    hi
-    ALIlcstem4-lcstem4
-    ",
-    # forbidden
-    "
-    enR
-    tides web
-    wc dzsuf
-    devnormal
-    DEVicon-tides
-    ALIicon-all
-    ",
-    1,
-    # rows
-    "
-    web
-    tides
-    SRCicon-([a-zA-Z]+)
-     ",
-    # cols
-    "
-    or-bi-fe.r([01]-[01])
-    t0-0DEV
-    ",
-    # sort
-    "no-reord",
-    0, # verbose
-    $valembeautify,
-  ],
-  [
-    "LM corpus size-simple-mteval",
-    "",
-    # required
-    "
-    ALIlcstem4-lcstem4
-    DEVicon-eilmt
-    ",
-    # forbidden
-    "
-    enR
-    tides
-    eiti
-    wc dzsuf
-    devnormal
-    DEVicon-tides
-    SRCicon-all\\+
-    ",
-    3,
-    # rows
-    "
-    SRCicon-([a-zA-Z0-9]+)
-    web
-     ",
-    # cols
-    "
-    or-bi-fe.r([01]-[01])
-    t0-0DEV di.r0-0DEV
-    ",
-    # sort
-    "reord-using-hi-forms",
-    0, # verbose
-    $valembeautify,
-  ],
-  [
-    "LM corpus size-simple",
-    "",
-    # required
-    "
-    ALIlcstem4-lcstem4
-    DEVicon-eilmt
-    ",
-    # forbidden
-    "
-    enR
-    tides
-    eiti
-    wc dzsuf
-    devnormal
-    DEVicon-tides
-    SRCicon-all\\+
-    ",
-    1,
-    # rows
-    "
-    SRCicon-([a-zA-Z0-9]+)
-    web
-     ",
-    # cols
-    "
-    or-bi-fe.r([01]-[01])
-    t0-0DEV di.r0-0DEV
-    ",
-    # sort
-    "reord-using-hi-forms",
-    0, # verbose
-    $valembeautify,
-  ],
-  [
-    "Parallel corpus size",
-    "",
-    # required
-    "
-    hi
-    ",
-    # forbidden
-    "
-    wc dzsuf
-    devnormal
-    DEVicon-tides
-    ",
-    1,
-    # rows
-    "
-    (ALI[^.]+)
-    LM([0-9]+-[0-9]+)
-    t0-0\\+t1-0
-    t0-0\\.
-    web
-    tides
-    or-bi-fe.r([01]-[01])
-     ",
-    # cols
-    "
-    SRCicon-([a-zA-Z]+)
-    ",
-    # sort
-    "SRCicon-eilmt",
-    0, # verbose
-    $valembeautify,
-  ],
-  [
-    "Normalization",
-    "",
-    # required
-    "
-    hi
-    ",
-    # forbidden
-    "
-    wc dzsuf
-    DEVicon-tides
-    ",
-    1,
-    # rows
-    "
-    SRCicon-([a-zA-Z]+)
-    (ALI[^.]+)
-    LM([0-9]+-[0-9]+)
-    web
-    tides
-    t0-0\\+t1-0
-    t0-0\\.
-    or-bi-fe.r([01]-[01])
-     ",
-    # cols
-    "
-    (hi\\.|devnormal2?)
-    ",
-    # sort
-    "hi.",
-    0, # verbose
-    $valembeautify,
-  ],
-  [
-    "Reorderings",
-    "",
-    # required
-    "
-    SRCicon-eilmt
-    lcstem4-lcstem4
-    ",
-    # forbidden
-    "
-    devnormal
-    web tides
-    Exiting
-    ",
-    1,
-    # rows
-    "
-    (ALI[^.]+)
-    LM([0-9]+-[0-9]+)
-    or-bi-fe.r([01]-[01])
-     ",
-    # cols
-    "
-    hi\\.
-    dzsuf
-    wc10
-    wc50
-    1130
-    ",
-    # sort
-    "SRCicon-eilmt",
-    0, # verbose
-    $valembeautify,
-  ],
-  [
-    "Reorderings - for paper",
-    "",
-    # required
-    "
-    lcstem4-lcstem4
-    ",
-    # forbidden
-    "
-    wc[15]0.*112[89]
-    LM1-[357]
-    devnormal
-    web 
-    Exiting
-    wc10 wc50
-    all eiti
-    LM0-3-icon-eilmtLM0-3-icon-tides
-    ",
-    1,
-    # rows
-    "
-    \\+en([RP]?)\\+
-    or-bi-fe.r([01]-[01])
-    dzsuf
-     ",
-    # cols
-    "
-    DEVicon-tides
-    DEVicon-eilmt
-    ",
-    # sort
-    "DEVicon-eilmt",
-    0, # verbose
-    undef,
-  ],
-  [
-    "Reorderings - simplified view, eilmt",
-    "",
-    # required
-    "
-    SRCicon-eilmt
-    lcstem4-lcstem4
-    ",
-    # forbidden
-    "
-    wc[15]0.*112[89]
-    LM1-[357]
-    devnormal
-    web tides
-    Exiting
-    ",
-    1,
-    # rows
-    "
-    \\+en([RP]?)\\+
-    or-bi-fe.r([01]-[01])
-     ",
-    # cols
-    "
-    hi\\.
-    dzsuf
-    wc10
-    wc50
-    ",
-    # sort
-    "SRCicon-eilmt",
-    0, # verbose
-    $valembeautify,
-  ],
-  [
-    "Reorderings - tides",
-    "",
-    # required
-    "
-    SRCicon-tides
-    lcstem4-lcstem4
-    ",
-    # forbidden
-    "
-    web
-    eiti
-    ",
-    1,
-    # rows
-    "
-    \\+en([RP]?)\\+
-    or-bi-fe.r([01]-[01])
-     ",
-    # cols
-    "
-    hi\\.
-    dzsuf
-    wc10
-    wc50
-    ",
-    # sort
-    "baseline",
-    0, # verbose
-    $valembeautify,
-  ],
-  [
-    "Reorderings - tides, trained on eiti",
-    "",
-    # required
-    "
-    SRCicon-eiti
-    lcstem4-lcstem4
-    DEVicon-tides
-    ",
-    # forbidden
-    "
-    web
-    ",
-    1,
-    # rows
-    "
-    (ALI[^.]+)
-    or-bi-fe.r([01]-[01])
-     ",
-    # cols
-    "
-    hi\\.
-    dzsuf
-    wc10
-    wc50
-    ",
-    # sort
-    "baseline",
-    0, # verbose
-    $valembeautify,
-  ],
+my $configfile;
+my @optionspecs = (
+  "f|config=s"=>\$configfile,
 );
 
+# use default options from ./augment.pl.flags, if available
+my $default_opt_file = "$basedir/$scriptname.flags";
+if (-e $default_opt_file) {
+  print STDERR "Loading default options from $default_opt_file\n";
+  my $h = my_open($default_opt_file);
+  my $defaultoptstr = "";
+  $defaultoptstr .= $_ while <$h>;
+  close $h;
+  GetOptionsFromString($defaultoptstr, @optionspecs)
+    or die "Bad options in $default_opt_file";
+  $configfile = File::Spec->rel2abs($configfile, dirname($default_opt_file));
+}
 
-# my @scans = @wmtscans;
+GetOptions(@optionspecs) or exit 1;
 
-foreach my $scan (@scans) {
+die "usage: $0 -f config.pl" if ! defined $configfile;
+my $config = require($configfile);
+
+die "Bad config in $configfile" if ! defined $config;
+
+my $data = Summarizer::load(*STDIN);
+
+foreach my $scan (@$config) {
   my ($title, $subtitle, $req, $forb, $col, $rows, $cols, $sortcol, $verbose, $tokenmap)
     = @$scan;
   Summarizer::newscan(
@@ -454,4 +61,34 @@ foreach my $scan (@scans) {
       tokenmap=>$tokenmap,
     },
     $data);
+}
+
+# end of main
+exit 0;
+
+
+
+sub my_open {
+  my $f = shift;
+  if ($f eq "-") {
+    binmode(STDIN, ":utf8");
+    return *STDIN;
+  }
+
+  die "Not found: $f" if ! -e $f;
+
+  my $opn;
+  my $hdl;
+  my $ft = `file $f`;
+  # file might not recognize some files!
+  if ($f =~ /\.gz$/ || $ft =~ /gzip compressed data/) {
+    $opn = "zcat $f |";
+  } elsif ($f =~ /\.bz2$/ || $ft =~ /bzip2 compressed data/) {
+    $opn = "bzcat $f |";
+  } else {
+    $opn = "$f";
+  }
+  open $hdl, $opn or die "Can't open '$opn': $!";
+  binmode $hdl, ":utf8";
+  return $hdl;
 }
