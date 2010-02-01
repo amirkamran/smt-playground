e941bc7ac9feadb1c7101fc84efaa8882b412dc8
bojar <bojar@85e76b90-d0e1-47da-8fde-e7818187306d>
2010-02-01 12:57:48 +0000

    a simple tool to get rid of Hajic's lemma suffixes
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/statmt/trunk@1114 85e76b90-d0e1-47da-8fde-e7818187306d

---
+++ scripts/strip_czech_lemma.pl
@@ -0,0 +1,19 @@
+#!/usr/bin/perl
+# strips funny suffixes of Czech lemmas
+# *including* word sense number, i.e. stát-1 ---> stát
+
+use strict;
+
+my $nr = 0;
+while (<>) {
+  $nr++;
+  chomp;
+  my @lems = split / /;
+  map {
+      s/(.)[-,;`_].*/$1/;
+      $_;
+    } @lems;
+  my $out = join(" ", @lems);
+  die "$nr:Lost word: $out" if $out =~ /  /;
+  print $out."\n";
+}
