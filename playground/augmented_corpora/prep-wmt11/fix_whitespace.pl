e5dc6d8a75ca757a79316f01e6df9d9d46b9c4fd
bojar <bojar@85e76b90-d0e1-47da-8fde-e7818187306d>
2011-02-13 00:15:00 +0000

    fixing various errors in inputs
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/statmt/trunk@1629 85e76b90-d0e1-47da-8fde-e7818187306d

--- playground/augmented_corpora/prep-wmt11/fix_whitespace.pl
+++ playground/augmented_corpora/prep-wmt11/fix_whitespace.pl
@@ -8,8 +8,8 @@ binmode(STDOUT, ":utf8");
 binmode(STDERR, ":utf8");
 
 while (<>) {
-  s/\x{a0}/ /g;
-  s/\s\s+/ /g;
+  s/[\x0d\x{a0}\x{f0b7}\x{f024}\x{2028}\x{2001}\x{2002}\x{2003}\x{2004}\x{2005}\x{2006}\x{2007}\x{2008}\x{2009}\x{200a}\x{3000}\x85]/ /g;
+  s/[ \t][ \t]+/ /g;
   s/^ //;
   s/ $//;
   print;
