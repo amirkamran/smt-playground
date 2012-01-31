0a1438ddcd936644dbbe5381d4f7883095c0e9ef
bojar <bojar@85e76b90-d0e1-47da-8fde-e7818187306d>
2012-01-31 00:07:17 +0000

    expect the user to specify which treex step to use
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/statmt/trunk@2740 85e76b90-d0e1-47da-8fde-e7818187306d

--- playground/tools/runtreex.sh
+++ playground/tools/runtreex.sh
@@ -7,7 +7,13 @@ set -o pipefail  # safer pipes
 mydir=$(dirname $(readlink -f "$0" ) )
 [ -d "$mydir" ] || die "Failed to find ourselves, got: $mydir"
 
-STEP=s.treex.ba0dc9c8.20120110-1456
+[ -e $mydir/runtreex.use-this-treex-step ] \
+|| die "Please put your desired s.treex.1234 step identifier into $mydir/runtreex.use-this-treex-step"
+
+STEP=$(cat $mydir/runtreex.use-this-treex-step)
+[ -z "$STEP" ] && die "No treex given in $mydir/runtreex.use-this-treex-step"
+[ -d "$mydir/../$STEP" ] || die "$STEP is not in $mydir/.."
+
 TREEX=$mydir/../$STEP/treex
 
 [ -e $TREEX.bashsource ] || die "$TREEX.bashsource not found"
