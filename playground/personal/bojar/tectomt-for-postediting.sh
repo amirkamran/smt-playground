b92242d0cc4581eb81cf00ad09798893ae86447b
bojar <bojar@85e76b90-d0e1-47da-8fde-e7818187306d>
2012-02-24 08:52:14 +0000

    a terrible results of tuning on lucselected preparing for tmt post-editing
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/statmt/trunk@2980 85e76b90-d0e1-47da-8fde-e7818187306d

--- playground/personal/bojar/tectomt-for-postediting.sh
+++ playground/personal/bojar/tectomt-for-postediting.sh
@@ -5,15 +5,31 @@
 function die() { echo "$@" >&2; exit 1; }
 set -o pipefail  # safer pipes
 
-find /net/cluster/TMP/czeng_translated_by_tectomt/ -type f -name '*streex' \
-| grep -v '[de]test' \
-| gzip -c > infilelist.gz
+dataset="$1"
+[ ! -z "$dataset" ] || die "usage: $0 czeng|newstest2010|..."
 
+if [ "$dataset" == czeng ]; then
+  find /net/cluster/TMP/czeng_translated_by_tectomt/ -type f -name '*streex' \
+  | grep -v '[de]test' \
+  | gzip -c > infilelist.gz
+  OUTCOLS=csm:csmtectomt
+  HACKSELECTOR=tectomt
+else
+  find /net/tmp/bojar/wmt12-bojar/playground/personal/bojar/wmt-translated-by-tectomt/$dataset \
+    -type f -name '*treex.gz' \
+  | gzip -c > infilelist.gz
+  OUTCOLS=csmtst
+  HACKSELECTOR=tst
+fi
+
+
+# the eval is a hack to fix a tectomt bug where "0" got set to ""
 
 qruncmd --jobs=20 --join " \
   source /net/tmp/bojar/wmt12-bojar/playground/s.treex.0274e2d1.20120223-0027/treex.bashsource ; \
   treex Read::Treex from=@- \
-    Write::Factored outcols=csm:csmtectomt \
+    Util::Eval language=cs selectors=$HACKSELECTOR anode='if (\$anode->form eq q()) { \$anode->set_form(q(0)); \$anode->set_lemma(q(0)); } \$anode->set_tag(q(X@-------------)) if (!defined \$anode->tag || \$anode->tag eq q());' \
+    Write::Factored outcols=$OUTCOLS \
     flags=escape_space:join_spaced_numbers \
   " infilelist.gz
 
