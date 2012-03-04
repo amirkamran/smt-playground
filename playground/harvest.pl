c08b1399656da2500a8e759628a285b6a6d29d2d
zeman <zeman@85e76b90-d0e1-47da-8fde-e7818187306d>
2012-03-04 21:24:55 +0000

    Other steps frendlier to eman continue. Harvesting BLEU scores from evaluator steps. WMT submission script now really works (a bit late, though :-)).
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/statmt/trunk@3029 85e76b90-d0e1-47da-8fde-e7818187306d

---
+++ playground/harvest.pl
@@ -0,0 +1,64 @@
+#!/usr/bin/env perl
+# Collects BLEU scores from evaluator steps.
+# Copyright © 2012 Dan Zeman <zeman@ufal.mff.cuni.cz>
+# License: GNU GPL
+
+use utf8;
+use open ':utf8';
+binmode(STDIN, ':utf8');
+binmode(STDOUT, ':utf8');
+binmode(STDERR, ':utf8');
+use dzsys;
+
+# Make sure that all evaluator steps have a tag that identifies the corpora used.
+dzsys::saferun("tagevals > /dev/null") or die;
+# Ask Eman to collect names, states, tags and scores of all evaluator steps.
+dzsys::saferun("eman collect") or die;
+# Read the results collected by Eman and sort them.
+open(RES, 'eman.results') or die("Cannot read eman.results: $!");
+while(<RES>)
+{
+    chomp();
+    my @fields = split(/\t/, $_);
+    # For some reason, eman.results contain several lines for each step.
+    # Only one of the lines contains the BLEU scores we are after.
+    next if($fields[2] =~ m/^(Snts|TAG)$/);
+    my %record =
+    (
+        'step'    => $fields[0],
+        'state'   => $fields[1],
+        'bleu'    => $fields[2],
+        'bleuint' => $fields[3],
+        'tags'    => $fields[4]
+    );
+    next unless($record{state} eq 'DONE');
+    if($record{tags} =~ m/SRCAUG=(.*?),TGTAUG=(.*?),/)
+    {
+        $record{pair} = "$1-$2";
+        # We may concern about factors later but currently there is always '+stc'.
+        $record{pair} =~ s/\+stc//g;
+    }
+    # We are only interested in the first combined tag ("A:SRCAUG..."). Erase the rest.
+    $record{tags} =~ s/^(A:SRCAUG=\S+).*$/$1/;
+    push(@results, \%record);
+}
+close(RES);
+@results = sort
+{
+    my $vysledek = $a->{pair} cmp $b->{pair};
+    unless($vysledek)
+    {
+        $vysledek = $b->{bleu} <=> $a->{bleu};
+    }
+    return $vysledek;
+}
+(@results);
+foreach my $r (@results)
+{
+    if($lastpair && $r->{pair} ne $lastpair)
+    {
+        print('-' x 80, "\n");
+    }
+    print("$r->{pair}\t$r->{bleu}\t$r->{bleuint}\t$r->{tags}\t$r->{step}\n");
+    $lastpair = $r->{pair};
+}
