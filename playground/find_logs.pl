7f52b09ba19995e03631f95e8a80ad4ddde46c6c
zeman <zeman@85e76b90-d0e1-47da-8fde-e7818187306d>
2012-09-27 19:41:25 +0000

    Bugfix; shortening unnecessarily long logs.
    git-svn-id: https://svn.ms.mff.cuni.cz/svn/statmt/trunk@3570 85e76b90-d0e1-47da-8fde-e7818187306d

--- playground/find_logs.pl
+++ playground/find_logs.pl
@@ -19,7 +19,7 @@ foreach my $step (@steps)
     # We are interested in steps only.
     next unless($step =~ m/^s\./);
     # Get the current status of the step. We will not remove or shorten logs of RUNNING and FAILED steps.
-    my $status = dzsys::qticks("cat $step/eman.status");
+    my $status = dzsys::qcticks("cat $step/eman.status");
     # Find logs in the step.
     opendir(DIR, $step) or die("Cannot read $step: $!");
     my @files = readdir(DIR);
@@ -31,6 +31,33 @@ foreach my $step (@steps)
         my $size = -s "$step/$file";
         my $lines = dzsys::chompticks("cat $step/$file | wc -l");
         push(@{$map{$step}}, {'status' => $status, 'file' => $file, 'size' => $size, 'lines' => $lines});
+        # Shorten long logs of DONE, OBSOLETE and ABOLISHED steps.
+        if($status =~ m/^(DONE|OBSOLETE|ABOLISHED)$/ && $lines>200)
+        {
+            my $tmp = "$file.$$";
+            print STDERR ("Reducing $step/$file, using temporary file $step/$tmp...\n");
+            open(IN, "$step/$file") or die("Cannot read $step/$file: $!");
+            open(OUT, ">$step/$tmp") or die("Cannot write $step/$tmp: $!");
+            my $i = 0;
+            while(<IN>)
+            {
+                if($i<50 || $i>=$lines-50)
+                {
+                    print OUT;
+                }
+                elsif($i==50)
+                {
+                    print OUT ("...\n");
+                }
+                $i++;
+            }
+            close(IN);
+            close(OUT);
+            dzsys::saferun("mv $step/$tmp $step/$file");
+            $total_logs_shortened++;
+            $total_lines_removed += $lines-100;
+            $total_bytes_removed += $size-(-s "$step/$file");
+        }
     }
 }
 # Print statistics.
@@ -62,3 +89,6 @@ foreach my $type (@types)
     print("TOTAL $type\t$typemap{$type}{steps} steps\t$typemap{$type}{logs} logs\t$typemap{$type}{size} bytes\t$typemap{$type}{lines} lines\n");
 }
 print("TOTAL\t$total_steps steps\t$total_logs logs\t$total_size bytes\t$total_lines lines\n");
+            $total_logs_shortened++;
+            $total_lines_removed += $lines-100;
+print("REMOVED\t$total_bytes_removed bytes\t$total_lines_removed lines\tfrom $total_logs_shortened logs\n");
