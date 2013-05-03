a838d7c834678eebb3e4b2fb13d0239eb94cbef4
Dan Zeman <zeman@ufal.mff.cuni.cz>
2013-05-03 23:42:28 +0200

    Improved detokenization.

--- playground/harvest.pl
+++ playground/harvest.pl
@@ -1,15 +1,32 @@
 #!/usr/bin/env perl
 # Collects BLEU scores from evaluator steps.
-# Copyright © 2012 Dan Zeman <zeman@ufal.mff.cuni.cz>
+# Copyright © 2012-2013 Dan Zeman <zeman@ufal.mff.cuni.cz>
 # License: GNU GPL
 
 use utf8;
+sub usage
+{
+    print STDERR ("Usage: harvest.pl [-usr <user> -psw <password>] > harvest.txt\n");
+    print STDERR ("Options:\n");
+    print STDERR ("       -usr: user name for matrix.statmt.org\n");
+    print STDERR ("       -psw: password\n");
+    print STDERR ("Creates a table of known results, sorted by languages, test sets and scores.\n");
+    print STDERR ("If the credentials for matrix.statmt.org are provided, submits best results there!\n");
+    print STDERR ("BEWARE: Understand what the submission scripts do before using them!\n");
+}
 use open ':utf8';
 binmode(STDIN, ':utf8');
 binmode(STDOUT, ':utf8');
 binmode(STDERR, ':utf8');
+use Carp;
+use Getopt::Long;
 use dzsys;
 
+GetOptions
+(
+    'usr=s' => \$usr,
+    'psw=s' => \$psw,
+);
 # Make sure that all evaluator steps have a tag that identifies the corpora used.
 dzsys::saferun("eman retag") or die;
 # Ask Eman to collect names, states, tags and scores of all evaluator steps.
@@ -86,14 +103,21 @@ foreach my $r (@results)
     {
         print('.' x 80, "\n");
     }
-    # Is this the best result for the given language pair and for the required test set?
-    if(!defined($lastpair) || $r->{pair} ne $lastpair || !defined($lasttest) || $r->{test} ne $lasttest)
+    # Submit results if required.
+    if($usr && $psw)
     {
-        # This is the first, i.e. best scored result for the given language pair and test set.
-        # Is it the required test set?
-        if($r->{test} eq 'wmt2013')
+        # Is this the best result for the given language pair and for the required test set?
+        if(!defined($lastpair) || $r->{pair} ne $lastpair || !defined($lasttest) || $r->{test} ne $lasttest)
         {
-            print('!'); ###!!!
+            # This is the first, i.e. best scored result for the given language pair and test set.
+            # Is it the required test set?
+            if($r->{test} eq 'wmt2013')
+            {
+                print('!'); # Mark submitted experiments in the harvest overview.
+                print STDERR ("Submitting $r->{step} ($r->{pair})...\n");
+                dzsys::saferun("presubmit.pl $r->{step} cu-zeman") or die;
+                dzsys::saferun("matrix_submit_results.pl -usr $usr -psw $psw -step $r->{step}") or die;
+            }
         }
     }
     print("$r->{pair}\t$r->{bleu}\t$r->{bleuint}\t$r->{tags}\t$r->{step}\n");
