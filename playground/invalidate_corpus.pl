ced2c2b534438049d2bd2f4b33d60a96922c8f25
Dan Zeman <zeman@ufal.mff.cuni.cz>
2013-12-23 18:15:21 +0100

    Upraveno invalidate_corpus.pl.

--- playground/invalidate_corpus.pl
+++ playground/invalidate_corpus.pl
@@ -25,6 +25,11 @@ use dzsys;
 
 
 # Get the name of the bad step. Does it exist? Is it a corpus step (is there a corpman.info file)?
+if(scalar(@ARGV) != 1)
+{
+    usage();
+    confess();
+}
 my $step0 = $ARGV[0];
 if(! -d $step0)
 {
@@ -34,4 +39,18 @@ if(! -f "$step0/corpman.info")
 {
     print STDERR ("WARNING: $step0 does not seem to be a corpus step. There is no corpman.info file.\n");
 }
-dzsys::saferun("eman tf $step0 --notree | sort -u") or confess();
+# First show the tree of descendants so that the user knows what is going to be invalidated.
+dzsys::saferun("eman tf $step0 --status --tag") or confess();
+print("ALL THE ABOVE STEPS WILL BE INVALIDATED. IF IN DOUBT, PRESS CTRL+C NOW.\n");
+dzsys::autoflush(*STDOUT); for(my $i = 10; $i > 0; $i--) { print("$i ... "); sleep(3); } print("GO!\n");
+my @steps = split(/\r?\n/, dzsys::safeticks("eman tf $step0 --notree | sort -u"));
+foreach my $step (@steps)
+{
+    dzsys::saferun("eman fail $step") or confess();
+    if(-f "$step/corpman.info")
+    {
+        dzsys::saferun("mv $step/corpman.info $step/corpman.info.bad") or confess();
+    }
+}
+print("TOTAL ", scalar(@steps), " steps.\n");
+dzsys::saferun('corpman reindex') or confess();
